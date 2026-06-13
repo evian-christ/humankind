29f7314f5a388ae3b4ab675fb1070a89c298b4c1
evian-christ <neoevia1@gmail.com>
2026-06-13 05:55:55 +0100

    Harden demo browser and window behavior

--- humankind-web/src/main.tsx
+++ humankind-web/src/main.tsx
@@ -4,6 +4,9 @@ import './index.css'
 import './settingsKeyBindings.css'
 import './crt.css'
 import { CrtRoot } from './CrtRoot.tsx'
+import { installBrowserBehaviorGuards } from './browserBehaviorGuards.ts'
+
+installBrowserBehaviorGuards()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
